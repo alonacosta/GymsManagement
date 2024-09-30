921f7b07dd6332fea69ee406390c1e36b112c1a7
Alona <alona.costa2@gmail.com>
2024-09-30 14:34:12 +0100

    GenericRepository

---
+++ GymManagement/Data/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GymManagement.Data
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<bool> ExistAsync(int id);
+    }
+}
